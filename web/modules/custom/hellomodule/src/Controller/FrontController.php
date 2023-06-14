f3400a9d4a694bd2ddeb0769f613eb406ef09d3f
ajay-mallah <ajay.mallah@innoraft.com>
2023-06-14 18:17:45 +0530

    FT2023-303: Getting Started with Custom Module
    1. Created frontcontroller for the greeting function.
    2. Added routing.yml file
    3. Added permissions.yml file

---
+++ web/modules/custom/hellomodule/src/Controller/FrontController.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Hello Module's front controller.
+ */
+
+namespace Drupal\hellomodule\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class FrontController extends ControllerBase {
+  /**
+   * @method greetUser()
+   *  Fetches current user and displays greet message.
+   */
+  public function greetUser() {
+    $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+    $user_name = $user->getAccountName();
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Hello @user', ['@user' => $user_name]),
+    ];
+  }
+}
